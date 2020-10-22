dd881d2ae2d226ef24e13ff7caae0588962985cd
hiba.fatima <hiba.fatima@tenpearls.com>
2020-10-22 20:47:39 +0500

    session#2 code

---
+++ src/test/java/myTests/pageClass.java
@@ -0,0 +1,28 @@
+package myTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class pageClass {
+
+    public WebDriver driver;
+
+    public WebDriver setup() {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+        return driver;
+    }
+
+    public String getMessage(String message){
+        WebElement messageText = driver.findElement(By.id("user-message"));
+        messageText.sendKeys(message);
+        WebElement button = driver.findElement(By.className("btn-default"));
+        button.click();
+        WebElement currentText = driver.findElement(By.id("display"));
+        String text = currentText.getText();
+        return text;
+    }
+}
